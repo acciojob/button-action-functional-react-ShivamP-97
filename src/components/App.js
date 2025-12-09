565ba01969ba090f3cc5a1dc4524b3eafdb88488
= <=>
2025-12-09 15:15:57 +0530

    helooo

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+    const [flag, setFlag] = useState(false)
+
+
   return (
     <div id="main">
-      // Do not alter the main div
+
+
+        <button id="click" onClick={()=>setFlag(true)}> Click </button>
+        {
+          flag &&  <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so</p>
+        }
+
     </div>
   );
 }
